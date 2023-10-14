e730f0fef1c2739653d51b3cf36c862990aa8d45
VISHNU <120297452+VISHNU2105204@users.noreply.github.com>
2023-10-14 21:34:35 +0530

    Create AccountBalance.java
    none

---
+++ AccountBalance.java
@@ -0,0 +1,22 @@
+package practice;
+
+class Account {
+	private double balance;
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	public java.lang.String toString() {
+		return "Account balance: " +balance;
+	}
+}
+class Main {
+	public static void main(String[] args) {
+		Account ac = new Account();
+		ac.deposit(25000);
+		ac.withdraw(3000);
+		System.out.println(ac);
+	}
+}
